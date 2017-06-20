ef637ff4c0e2eebf51d8912e9a4b555c70cf72f6
KevFan <kevin_fan@hotmail.co.uk>
2017-06-20 16:06:10 +0100

    assessment store + load assessment data from json

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -2,14 +2,18 @@
 
 const logger = require('../utils/logger');
 
+const listOfAssessments = require('../models/assessment-store');
+
 const dashboard = {
-  index(request, response) {
-    logger.info('dashboard rendering');
-    const viewData = {
-      title: 'Node Gym',
-    };
-    response.render('dashboard', viewData);
-  },
+    index(request, response) {
+        logger.info('dashboard rendering');
+        const viewData = {
+            title: 'Playlist Dashboard',
+            assessmentlist: listOfAssessments,
+        };
+        logger.info('about to render', listOfAssessments);
+        response.render('dashboard', viewData);
+    },
 };
 
 module.exports = dashboard;
---
+++ models/assessment-store.js
@@ -0,0 +1,10 @@
+/**
+ * Created by kevin on 20/06/2017.
+ */
+'use strict';
+
+const logger = require('../utils/logger');
+
+const listOfAssessments = require('./assessment-store.json');
+
+module.exports = listOfAssessments;
