28b468fe7cfbc200217847432977cb09c168354f
KevFan <kevin_fan@hotmail.co.uk>
2017-07-06 13:52:30 +0100

    Accounts controller and dashboard changes for session support

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -1,22 +1,24 @@
 'use strict';
 
 const logger = require('../utils/logger');
-
 const assessmentStore = require('../models/assessment-store');
 const uuid = require('uuid');
+const accounts = require('./accounts.js');
 
 const dashboard = {
   index(request, response) {
     logger.info('dashboard rendering');
+    const loggedInUser = accounts.getCurrentUser(request);
     const viewData = {
       title: 'Playlist Dashboard',
-      assessmentlist: assessmentStore.listOfAssessments,
+      assessmentlist: assessmentStore.getAssessmentList(loggedInUser.id),
     };
-    logger.info('about to render', assessmentStore.listOfAssessments);
+    logger.info('about to render user: ', assessmentStore.getAssessmentList(loggedInUser.id));
     response.render('dashboard', viewData);
   },
 
   addAssessment(request, response) {
+    const loggedInUser = accounts.getCurrentUser(request);
     const newAssessment = {
       id: uuid(),
       weight: Number(request.body.weight),
@@ -26,14 +28,14 @@ const dashboard = {
       waist: Number(request.body.waist),
       hips: Number(request.body.hips),
     };
-    assessmentStore.addAssessment(newAssessment);
+    logger.debug('New Assessment = ', newAssessment);
+    assessmentStore.addAssessment(loggedInUser.id, newAssessment);
     response.redirect('/dashboard/');
   },
 
   deleteAssessment(request, response) {
-    const assessmentId = request.params.assessmentid;
-    logger.debug(`Deleting Assessment ${assessmentId}`);
-    assessmentStore.removeAssessment(assessmentId);
+    const loggedInUser = accounts.getCurrentUser(request);
+    assessmentStore.removeAssessment(loggedInUser.id, request.params.assessmentid);
     response.redirect('/dashboard/');
   },
 };
